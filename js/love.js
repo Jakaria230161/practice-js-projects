b168edfdc4895bad5bb65c4b8d431f964438567f
Jakaria230161 <jakariamahmud016@gmail.com>
2022-08-19 13:07:07 +0600

    love you

---
+++ js/love.js
@@ -0,0 +1,8 @@
+document.getElementById("yes-btn").addEventListener("click", function () {
+    document.getElementById("msg").innerText = "I love you so much! i wanna marry you within only two months In Sha Allah!";
+})
+
+
+document.getElementById("of-course-btn").addEventListener("click", function () {
+    document.getElementById("msg").innerText = "I love you so much!";
+})
